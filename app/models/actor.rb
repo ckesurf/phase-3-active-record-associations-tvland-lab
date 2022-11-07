2519a62551da4c11abb68db8e11110e67122e7ca
Chris Erlendson <ckesurf@gmail.com>
2022-11-07 18:12:07 -0500

    commit tree 8760af1d539292c1a3f06f5a8c5444aa5afa7370

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 class Actor < ActiveRecord::Base
-  
+  # association macro
+  has_many :characters
+  has_many :shows, through: :characters
+
+
+  def full_name
+    "#{self.first_name} #{last_name}"
+  end
+
+  # @return [Array] of character names and show names
+  def list_roles
+    self.characters.map do |c|
+      "#{c.name} - #{c.show.name}"  
+    end 
+  end
+
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 class Character < ActiveRecord::Base
-  
+  # c.actor
+  belongs_to :actor
+  belongs_to :show
+  # Batman: The Dark Knight -> Christian Bale
+  # Tyrion Lannister -> Peter Dinkelage
+  # Captain America -> Chris Evans
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
 end
